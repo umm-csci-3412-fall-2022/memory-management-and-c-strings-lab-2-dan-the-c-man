2587427d841e189b583d82424a97291c5d69dc56
Daniel Albright <albri118@morris.umn.edu>
2022-09-29 16:04:49 -0500

    add array of vowels

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -3,5 +3,12 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+
+  char vowels[10] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+  for (int i=0; i<strlen(string); i++) {
+    
+  }
+
+  return result;
 }
